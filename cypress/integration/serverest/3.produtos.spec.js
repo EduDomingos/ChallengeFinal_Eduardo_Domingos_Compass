c1c93f62be707cf197798847102cd7925cd20302
EduDomingos <88852683+EduDomingos@users.noreply.github.com>
2022-09-15 16:47:51 -0300

    adicionando arquivos de rota login e produtos

---
+++ cypress/integration/serverest/3.produtos.spec.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+import Serverest from '../../services/serverest.service'
+import ValidaServerest from '../../services/validaServerest.service'
+
+describe('Casos de teste sobre a rota /produtos da API Serverest', () => {
+  
+
+})
