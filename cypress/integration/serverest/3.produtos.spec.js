0f26a1cb7f6568f0da392e1025e65343cd09f7ce
EduDomingos <88852683+EduDomingos@users.noreply.github.com>
2022-09-19 17:56:08 -0300

    Correções da aula 06

--- cypress/integration/serverest/3.produtos.spec.js
+++ cypress/integration/serverest/3.produtos.spec.js
@@ -11,16 +11,37 @@ describe('Casos de teste sobre a rota /produtos da API Serverest', () => {
     })
   })
 
-  it('Deve postar um novo produto com sucesso', ()=>{
-    Serverest.buscarUsuarioParaLogin()
-    cy.get('@usuarioLogin').then( usuario => {
-      Serverest.logar(usuario).then( res =>{
-        ValidaServerest.validaLoginComSucesso(res)
-        Serverest.salvarBearer(res)
-        Serverest.cadastrarProdutoComSucesso().then(res =>{
-          ValidaServerest.validarCadastroDeProdutoComSucesso(res)
+  // it('Deve postar um novo produto com sucesso', ()=>{
+    //   Serverest.buscarUsuarioParaLogin()
+    //   cy.get('@usuarioLogin').then( usuario => {
+      //     Serverest.logar(usuario).then( res =>{
+        //       ValidaServerest.validaLoginComSucesso(res)
+        //       Serverest.salvarBearer(res)
+        //       Serverest.cadastrarProdutoComSucesso().then(res =>{
+          //         ValidaServerest.validarCadastroDeProdutoComSucesso(res)
+          //       })
+          //     })
+          //   })
+          // })
+
+  context('Logar com sucesso', () => {
+    beforeEach('Logar', () => {
+      Serverest.buscarUsuarioParaLogin()
+      cy.get('@usuarioLogin').then( usuario => {
+        Serverest.logar(usuario).then( res => {
+          ValidaServerest.validaLoginComSucesso(res)
+          Serverest.salvarBearer(res)
         })
       })
     })
+
+    it('Deve postar um novo produto com sucesso', ()=>{
+      Serverest.cadastrarProdutoComSucesso().then(res => {
+        ValidaServerest.validarCadastroDeProdutoComSucesso(res)
+      })
+    })
+
   })
+
+
 })
--- cypress/services/serverest.service.js
+++ cypress/services/serverest.service.js
@@ -9,49 +9,67 @@ export default class Serverest {
   Cadastrar novos usuários
   Efetuar login*/
 
-    static buscarUsuarios() {
-        return cy.rest(URL_USUARIOS)
-    }
+  static buscarUsuarios() {
+      return cy.rest(URL_USUARIOS)
+  }
 
-    static buscarUsuarioParaLogin() {
-      cy.request(URL_USUARIOS).then( res => {
-        cy.wrap({
-          email: res.body.usuarios[2].email,
-          password: res.body.usuarios[2].password
-        }).as('usuarioLogin')
-      })
-    }
+  static buscarUsuarioParaLogin() {
+    cy.request(URL_USUARIOS).then( res => {
+      cy.wrap({
+        email: res.body.usuarios[2].email,
+        password: res.body.usuarios[2].password
+      }).as('usuarioLogin')
+    })
+  }
 
-    static logar(usuario){
-      return cy.rest(URL_LOGIN, 'POST', usuario)
-    }
+  static logar(usuario){
+    return cy.rest(URL_LOGIN, 'POST', usuario)
+  }
 
-    static salvarBearer(res){
-      Cypress.env('bearer', res.body.authorization)
-      cy.log("bearer: "+ Cypress.env('bearer').slice(7))
-    }
+  static salvarBearer(res){
+    Cypress.env('bearer', res.body.authorization.slice(7))
+    cy.log("bearer: "+ Cypress.env('bearer').slice(7))
+  }
 
 // Produtos //
 
-    static buscarProdutos(){
-      return cy.rest(URL_PRODUTOS)
-    }
+  static buscarProdutos(){
+    return cy.rest(URL_PRODUTOS)
+  }
 
-    static cadastrarProdutoComSucesso(){
-      return cy.request({
-        method: 'POST',
-        url: URL_PRODUTOS,
-        failOnStatusCode: true,
-        headers: {
-          authorization: Cypress.env('bearer')
-        },
-        body: {
-          nome: "Telescópio x1000",
-          preco: "1000",
-          descricao: "lentes 56pkm",
-          quantidade: 12
-        }
-      })
-    }
+  // static cadastrarProdutoComSucesso(){
+  //   return cy.request({
+  //     method: 'POST',
+  //     url: URL_PRODUTOS,
+  //     failOnStatusCode: true,
+  //     headers: {
+  //       authorization: Cypress.env('bearer')
+  //     },
+  //     body: {
+  //       nome: "Telescópio x1000",
+  //       preco: "1000",
+  //       descricao: "lentes 56pkm",
+  //       quantidade: 12
+  //     }
+  //   })
+  // }
+
+  static cadastrarProdutoComSucesso(){
+    return cy.request({
+      method: 'POST',
+      url: URL_PRODUTOS,
+      body: {
+        "nome": "Almofada top",
+        "preco": 33,
+        "descricao": "Almofada",
+        "quantidade": 195
+      },
+      failOnStatusCode: true,
+      auth: {
+        bearer: Cypress.env('bearer')
+      }
+
+    })
+  }
 
 }
