e158f9adbd5aeadc6563f9d6322135bb14f3b9e3
EduDomingos <88852683+EduDomingos@users.noreply.github.com>
2022-09-20 20:58:49 -0300

    Deleção de arquivos de exemplo

--- cypress/integration/test.spec.js
+++
@@ -1,6 +0,0 @@
-it('Deve realizar uma requesição GET na rota /usuarios', () => {
-  cy.request('/usuarios').then(res => {
-    expect(res).to.be.a('object')
-    cy.log(JSON.stringify(res))
-  })
-})
